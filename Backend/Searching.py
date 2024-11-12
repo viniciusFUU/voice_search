f8d31fdfdbe32e85ee8a4a800ad253a30075000c
Vinícius Monteiro <vinicius3_1998@hotmail.com>
2024-11-12 15:33:57 -0300

    Ajustando o self por cls (classmethod)

--- Backend/Searching.py
+++ Backend/Searching.py
@@ -4,14 +4,15 @@ import lxml
 import os
 
 class Searching:
-    def wikipedia_search(self, arquivo):
-
+    @classmethod
+    def wikipedia_search(cls, arquivo):
         caminho_textos = os.path.join('Backend', 'textos', arquivo)
         
         with open(caminho_textos, 'r') as busca:
             texto = busca.read().strip()
             
         caminho_pesquisa = os.path.join('Backend', 'Pesquisas')
+        print(f"esse é o caminho: {caminho_pesquisa}")
         
         url = f"https://pt.wikipedia.org/wiki/{texto.replace(' ', '_')}"
         response = requests.get(url)
@@ -20,20 +21,15 @@ class Searching:
 
         resposta = [p.get_text() for p in paragrafos[:3]]
 
-        self.verificacao_pasta_pesquisa(caminho_pesquisa)
+        cls.verificacao_pasta_pesquisa(caminho_pesquisa)
 
-        with open(os.path.join('Backend', 'Pesquisas',f'{texto}.txt'), 'w') as pesquisa:
+        with open(os.path.join('Backend', 'Pesquisas',f'{texto}.txt'), 'w', encoding="utf-8") as pesquisa:
             pesquisa.write("\n".join(resposta))
 
         return resposta
 
-    def verificacao_pasta_pesquisa(self, caminho_pesquisa):
-        existPesquisas = []
-
-        caminho = os.path.join('Backend')
-
+    @classmethod
+    def verificacao_pasta_pesquisa(cls, caminho_pesquisa):
         if not os.path.exists(caminho_pesquisa):
             print("Criando a pasta 'Pesquisas'")
             os.makedirs(caminho_pesquisa)
-
-Searching().wikipedia_search('arquivo1.txt')
