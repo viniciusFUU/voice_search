29896b8313fa2c2478468ec844ce3dd489c851cd
Vinicius <vinicius3_1998@hotmail.com>
2024-11-11 21:03:48 -0300

    ajustes no contador do json

--- Backend/autoAudiosWriter.py
+++ Backend/autoAudiosWriter.py
@@ -29,27 +29,26 @@ class AutoAudiosWriter:
         with open(caminho_contador, 'r') as contador:
             contador = json.load(contador)
         
-        nome_arquivo = f"arquivo{contador['contador']}.txt"
+        nome_arquivo = f"arquivo{contador['contador']}"
         print(nome_arquivo)
         # texto = cls.autoAudiosWritter()
-        texto = "brasil"
+        texto = "corinthians"
 
-        caminho_completo = os.path.join('Backend', 'textos', nome_arquivo)
+        caminho_completo = os.path.join('Backend', 'textos', f"{nome_arquivo}.txt")
 
         with open(caminho_completo, 'w', encoding='utf-8') as arquivo:
             arquivo.write(texto)
         
         print("Documento criado com sucesso.")
 
-        wiki_pesquisa = Searching.Searching.wikipedia_search(nome_arquivo)
-        print(f"primeira pesquisa{wiki_pesquisa}")
-        wiki_pesquisa = " ".join(wiki_pesquisa)
-        print(f"segunda pesquisa{wiki_pesquisa}")
-        audio_player.AudioPlayer.reproduzir_audio(f"arquivo{contador['contador']}", wiki_pesquisa)
-
-        contador['contador']+=1
-
         with open(caminho_contador, 'w') as arquivo_contador:
+            print(contador)
+            contador['contador']+=1
+            print(contador)
             json.dump(contador, arquivo_contador, ensure_ascii=False, indent=4)
+
+        wiki_pesquisa = Searching.Searching.wikipedia_search(f"{nome_arquivo}.txt")
+        wiki_pesquisa = " ".join(wiki_pesquisa)
+        audio_player.AudioPlayer.reproduzir_audio(nome_arquivo, wiki_pesquisa)
 
 AutoAudiosWriter.criar_bloco_de_notas()
