3722d59bb1aea076b1b940c76617bf293a64e443
Vinícius Monteiro <vinicius3_1998@hotmail.com>
2024-11-12 14:17:05 -0300

    Adicionando função de abrir uma pesquisa e adicionando função de reproduzir o audio

--- Frontend/Application.py
+++ Frontend/Application.py
@@ -4,8 +4,8 @@ import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 import tkinter as tk
-from tkinter import ttk
-from Backend import Audios_controller, Textos_controller
+from tkinter import ttk, messagebox, Message
+from Backend import Audios_controller, Textos_controller, Pesquisas_controller, AudioPlayer
 
 root = tk.Tk()
 
@@ -16,7 +16,8 @@ class Application:
 
     def tela(self):
         root.title('programa')
-        
+        root.config(background='#260d33')
+
         largura = 1000
         altura = 650
 
@@ -29,27 +30,57 @@ class Application:
         self.combo_box_audios()
 
     def combo_box_textos(self):
-        ttk.Label(root, text = "   Textos   ",  
-          background = 'red', foreground ="white",  
-          font = ("Times New Roman", 15)).grid(row = 0, column = 1) 
         
-        opcoes = Textos_controller.Textos_controller.adc_textos_lista()
+        ttk.Label(
+                root, 
+                text = "Pesquisas",
+                anchor= 'center',  
+                background = '#157a8c', 
+                foreground ="white",  
+                font = ("Times New Roman", 15)
+                ).grid(row = 1, column = 1, pady=(10, 0), ipadx=31) 
+        
+        opcoes = Pesquisas_controller.Pesquisas_controller.listar_arquivos()
 
         combobox = ttk.Combobox(root, values=opcoes)
         combobox.set("Escolha uma opção")
-        combobox.grid(row=1, column=1, padx=10, pady=10)
+        combobox.grid(row=2, column=1, padx=10, pady=5)
+
+        tk.Button(root, text="Abrir texto").grid(row=3, column=1, pady=(0,5), ipadx=38)
+
+        def abrir_texto():
+            valor_escolhido = combobox.get()  
+            if valor_escolhido != "Escolha uma opção":
+                messagebox.showinfo('Conteudo encontrado',Pesquisas_controller.Pesquisas_controller.busca_pesquisa(valor_escolhido))
+            else:
+                messagebox.showinfo('Erro: ', "Nenhum elemento selecionado")            
+
+        tk.Button(root, text="Abrir texto", command=abrir_texto).grid(row=3, column=1, pady=(0,5), ipadx=38)
 
     def combo_box_audios(self):
-        ttk.Label(root, text = "   Audios   ",  
-          background = 'red', foreground ="white",  
-          font = ("Times New Roman", 15)).grid(row = 0, column = 2) 
+        ttk.Label(
+                root, 
+                text = "Audios",
+                anchor= 'center',  
+                background = '#157a8c', 
+                foreground ="white",  
+                font = ("Times New Roman", 15)
+                ).grid(row = 4, column = 1, pady=(5, 0), ipadx=41) 
         
         opcoes = Audios_controller.Audios_controller.adc_audios_lista()
 
-        print(opcoes)
-
         combobox = ttk.Combobox(root, values=opcoes)
         combobox.set("Escolha uma opção")
-        combobox.grid(row=1, column=2, padx=10, pady=10)
+        combobox.grid(row=5, column=1, padx=10, pady=5)
+
+        def abrir_audio():
+            valor_escolhido = combobox.get()
+            if valor_escolhido != "Escolha uma opção":
+                print(valor_escolhido)
+                AudioPlayer.AudioPlayer.reproduzir_audio(valor_escolhido)
+            else:
+                messagebox.showinfo("Erro: ", "Nenhum elemento selecionado")
+
+        tk.Button(root, text="Escutar audio", command=abrir_audio).grid(row=6, column=1, pady=(0,5), ipadx=31)
 
 Application()
